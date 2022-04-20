05678471188de431b8fa8f572400ad84beae0d67
DikiMartin <104083023+DikiMartin@users.noreply.github.com>
2022-04-20 21:30:40 +0700

    Add files via upload

---
+++ run1.py
@@ -0,0 +1,10 @@
+# menghasilkan bilangan genap - 0,2,4,6,8,10,12,14
+# Nama : Diki Martin
+# Nim  : 191011400319
+# Kelas: 06tplom003
+
+for i in range(15):
+    # cek jika i bilangan genap
+    if i % 2 == 1:
+        continue
+    print(i)
